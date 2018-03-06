71dccdf1caf30e1451f1bdf901b335e50ffe06dc
CharlieCodex <charlie@codexvt.com>
2018-03-06 06:55:25 -0500

    March 6 update; its been a while

---
+++ cpp/lisp-like/lisp-like/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lisp-like
+//
+//  Created by Charles McVicker on 26/01/2018.
+//  Copyright © 2018 Charles McVicker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
